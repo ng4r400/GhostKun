bb3c5b8e3a7acdb957ac01b2ce434b408cc04cf7
ng4r400 <45040543+ng4r400@users.noreply.github.com>
2018-11-17 20:25:26 +0700

    Add files via upload

---
+++ 31-in-1-tools.sh
@@ -0,0 +1,1203 @@
+#system/bin/sh
+
+#Create Tools By Cyber Indonesia
+
+clear
+
+
+#variables
+
+blue="\033[34;1m"
+
+green="\033[32;1m"
+
+purple="\033[35;1m"
+
+cyan="\033[36;1m"
+
+red="\033[31;1m"
+
+white="\033[37;1m"
+
+yellow="\033[33;1m"
+
+normal="\033[0m"
+
+cd cuk
+
+python allq.py
+
+clear
+
+echo ""
+
+echo -e $red "L......."
+
+sleep 0.3
+
+clear
+
+echo -e $white"LO............."
+
+sleep 0.3
+
+clear
+
+echo -e $green"LOA..................."
+
+sleep 0.3
+
+clear
+
+echo -e $cyan"LOAD........................"
+
+sleep 0.3
+
+clear
+
+echo -e $yellow"LOADI............................"
+
+sleep 0.3
+
+clear
+
+echo -e $blue"LOADIN...................................."
+
+sleep 0.3
+
+clear
+
+echo -e $purple"LOADING....................................."
+
+sleep 0.3
+
+clear
+
+echo ""
+
+echo -e $red "L......."
+
+sleep 0.3
+
+clear
+
+echo -e $white"LO.........."
+
+sleep 0.3
+
+clear
+
+echo -e $green"LOA..............."
+
+sleep 0.3
+
+clear
+
+echo -e $cyan"LOAD....................."
+
+sleep 0.3
+
+clear
+
+echo -e $yellow"LOADI........................."
+
+sleep 0.3
+
+clear
+
+echo -e $blue"LOADIN................................"
+
+sleep 0.3
+
+clear
+
+echo -e $purple"LOADING...................................."
+
+sleep 0.3
+
+clear
+
+echo ""
+
+echo -e $red "L......."
+
+sleep 0.3
+
+clear
+
+echo -e $white"LO............."
+
+sleep 0.3
+
+clear
+
+echo -e $green"LOA..................."
+
+sleep 0.3
+
+clear
+
+echo -e $cyan"LOAD.........................."
+
+sleep 0.3
+
+clear
+
+echo -e $yellow"LOADI.............................."
+
+sleep 0.3
+
+clear
+
+echo -e $blue"LOADIN..................................."
+
+sleep 0.3
+
+clear
+
+echo -e $purple"LOADING...................................."
+
+sleep 0.3
+
+clear
+
+echo ""
+
+echo -e $red "L......."
+
+sleep 0.3
+
+clear
+
+echo -e $white"LO..........."
+
+sleep 0.3
+
+clear
+
+echo -e $green"LOA.................."
+
+sleep 0.3
+
+clear
+
+echo -e $cyan"LOAD........................."
+
+sleep 0.3
+
+clear
+
+echo -e $yellow"LOADI............................"
+
+sleep 0.3
+
+clear
+
+echo -e $blue"LOADIN.................................."
+
+sleep 0.3
+
+clear
+
+echo -e $purple"LOADING....................................."
+
+sleep 0.3
+
+clear
+
+figlet -f shadow Loading Succes
+
+sleep 2
+
+clear
+
+echo -e $red " mmm"
+
+echo -e $red " ####"
+
+echo -e $red " ##"
+
+echo -e $red " ##"
+
+echo -e $red " ##"
+
+echo -e $red " mmm##mmm"
+
+echo -e $red " ========"
+
+sleep 3
+
+clear
+
+echo -e $blue" mmmmm"
+
+echo -e $blue" #mm m##m"
+
+echo -e $blue" mmm ##"
+
+echo -e $blue" m#m"
+
+echo -e $blue" m#m"
+
+echo -e $blue" m##mmmmm"
+
+echo -e $blue" mmmmmmmm "
+
+sleep 3
+
+clear
+
+echo -e $green" mmmmm"
+
+echo -e $green" #mmmm##m"
+
+echo -e $green" m##"
+
+echo -e $green" #####"
+
+echo -e $green" m##"
+
+echo -e $green" #mmmm##m"
+
+echo -e $green" mmmmm"
+
+sleep 4
+
+clear
+
+figlet -f big AllQ CybeR
+
+sleep 4
+
+clear
+
+sleep 4
+
+echo -e $cyan""
+
+cat coba.txt
+
+echo -e $cyan" _______ _______ _______ _______ _______ _______ ______ _______"
+
+echo -e $cyan" | __|| ___|| ___|| __||_ _|| ___|| __ \| __|"
+
+echo -e $cyan" |__ || ___|| ___||__ | | | | ___|| <|__ |"
+
+echo -e $cyan" |_______||_______||___| |_______| |___| |_______||___|__||_______|"
+
+
+echo -e $green ""
+
+echo "╔═════════════════════════════════════════════════════════════════════════════╗"
+
+echo "║╔═══════════════════════════════════════════════════════════════════════════╗║"
+
+echo "║║[÷]Author : GhostName ║║"
+& Asep Sefsters's
+echo "║║ ║║"
+
+echo "║║[÷]Thanks To : Indonesia Termux Association,J.C.A And ALLQ ║║"
+
+echo "║║ ║║"
+
+echo "║║[÷]Contact Me : 089678211875 ║║"
+
+
+echo "║║ ║║"
+
+echo "║║[÷]Tools : Tools Installer ║║"
+
+echo "║║ ║║"
+
+echo "║║[÷]Bio : Gua Mau Nanya ? Gimana Hack Hati Perempuan ? :( ║║"
+
+echo "║║ Tools ini semoga Bermanfaat Dan gunakan Dwngan Bijak ║║"
+
+echo "║║ Hargai perjuangan orang lain :) ║║"
+
+echo "║╚═══════════════════════════════════════════════════════════════════════════╝║"
+
+echo "╚═════════════════════════════════════════════════════════════════════════════╝"
+
+
+##############################
+
+# CTRL C
+
+#############################
+
+trap ctrl_c INT
+
+ctrl_c() {
+
+clear
+
+echo -e $cyan"[×]Ctrl-C terdeteksi Tod..."
+
+sleep 1
+
+echo ""
+
+echo -e $cyan"[×]Selamat Datang Di Tools Kami:)"
+
+sleep 1
+
+echo ""
+
+echo -e $cyan"[×]Selamat Mencoba Dan Selamat Menikmati Toolsnya"
+
+sleep 3
+
+echo ""
+
+sleep 7
+
+exit
+
+}
+
+
+echo ""
+
+echo -e $red"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[1]. install TUANB4DUT"
+
+echo -e $blue"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[2]. installBAJINGANv6"
+
+echo -e $green"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[3]. install MR.CAKIL"
+
+echo -e $yellow"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[4]. install Metasploit-Framework"
+
+echo -e $purple"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[5]. install Lazysqlmap"
+
+echo -e $white"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[6]. install SQLMAP (buat inject situs)"
+
+echo -e $cyan"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[7]. install WhatsHack (buat hack wa)"
+
+echo -e $red"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[8]. install Tembak Kuota"
+
+echo -e $blue"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[9]. Install ipSceen (sadap CCTV)"
+
+echo -e $green"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[10]. install Defacer-V2"
+
+echo -e $yellow"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[11]. install DDOS (serang website)"
+
+echo -e $purple"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[12]. install REDHAWK"
+
+echo -e $white"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[13]. Install Malicious (game viruz)"
+
+echo -e $cyan"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[14]. install Deface Creator (bikin script)"
+
+echo -e $red"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[15]. Install Weeman (phising)"
+
+echo -e $blue"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[16]. Install Phising Game Online"
+
+echo -e $green"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[17]. Install BruteWebDav"
+
+echo -e $yellow"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[18]. Install VulnScanner"
+
+echo -e $purple"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[19]. Install WebDav"
+
+echo -e $white"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[20]. Install Admin Finder "
+
+echo -e $cyan"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[21]. Install Wifite (hack wifi khusus RooT)"
+
+echo -e $red"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[22]. install A-Rat (hack android)"
+
+echo -e $blue"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[23]. install mbf (Hack FB)"
+
+echo -e $green"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[24]. Install multi-bruteforcr-facebook(Hack Narget FB"
+
+echo -e $yellow"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[25]. install Crack Medsos"
+
+echo -e $purple"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[26]. install OSIF (Informasi Gathering Facebook "
+
+echo -e $white"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[27]. install DarkFly-Tool's"
+
+echo -e $cyan"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[28]. install SpamSMS"
+
+echo -e $red"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[29]. install Katoolin"
+
+echo -e $blue"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[30]. install PrankCall"
+
+echo -e $green"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo -e "[31]. install nuyul kubik (buat pecinta receh :v )"
+
+echo -e $yellow"✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏✏"
+
+echo ""
+
+echo -e $cyan" ╔═══════════════════════╗"
+
+echo -e $green" ║ No Sistem Is Save ! ║"
+
+echo -e $cyan" ╚═══════════════════════╝"
+
+echo -e $green
+
+echo -e "╭GhostName & Root./SEFSTER'S"
+
+echo -e "│"
+
+read -p "╰─────────────────【AllQ】" bro;
+
+
+case $bro in
+
+1) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install git
+
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+
+cd TOOLSINSTALLERv4
+
+chmod 777 TUANB4DUT..sh
+
+./TUANB4DUT..sh
+
+
+;;
+
+
+
+2) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install termcolor
+
+pkg install php
+
+pip2 install lolcat
+
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+
+cd BAJINGANv6
+
+sh BAJINGAN.sh
+
+
+;;
+
+
+
+3) figlet -f shadow installing...
+
+apt update && apt upgrade
+
+apt install git
+
+git clone https://github.com/Mrcakil/mrcakil
+
+cd mrcakil
+
+chmod +x tools
+
+./tools
+
+
+;;
+
+
+
+4) figlet -f shadow installing...
+
+pkg install git
+
+pkg install python3
+
+pkg install ruby
+
+pkg install wget
+
+pkg install curl
+
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+chmod +x metasploit.sh
+
+bash metasploit.sh
+
+cd metasploit-framework/
+
+bundle install
+
+gem install nokogiri
+
+bundle install nokogiri
+
+gem install nokogiri -- --use-system-libraries
+
+pkg-config
+
+bundle update nokogiri
+
+./msfconsole
+
+
+;;
+
+
+
+
+5)figlet -f shadow installing...
+
+pkg install git
+
+git clone https://github.com/verluchie/termux-lazysqlmap
+
+cd termux-lazysqlmap
+
+chmod 777 install.sh
+
+./install.sh
+
+
+;;
+
+
+6) figlet -f shadow installing...
+
+pkg install python2 -y
+
+pkg install gnupg
+
+pkg install php python
+
+pkg install python python2 vim figlet curl
+
+pkg install bash
+
+pkg install curl
+
+apt install openssl curl
+
+pkg install libcurl
+
+git clone https://github.com/sqlmapproject/sqlmap.git
+
+cd sqlmap
+
+python2 sqlmap.py
+
+
+;;
+
+
+
+7) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install nano
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/AndriGanz/whatshack
+
+cd whatshack
+
+sh whatshack.sh
+
+
+;;
+
+
+
+8) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install git
+
+pkg install python python2 vim figlet curl
+
+git clone https://github.com/Bintang73/tembaktembakan.git
+
+cd tembaktembakan
+
+python dor.py
+
+
+;;
+
+
+9) figlet -f shadow installing...
+
+pkg install python
+
+apt-get install git
+
+gem install lolcat
+
+pkg install git
+
+git clone https://github.com/kancotdiq/ipcs
+
+cd ipcs
+
+python2 scan.py
+
+
+;;
+
+
+
+10) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install curl
+
+pkg install git
+
+git clone https://github.com/21D4N404/Defacer-V2
+
+cd Defacer-V2
+
+sh Defacer-V2.sh
+
+
+;;
+
+
+
+11) figlet -f shadow installing...
+
+pkg install python
+
+pkg install curl
+
+pkg install git
+
+pkg install php
+
+git clone https://github.com/cyweb/hammer
+
+cd hammer
+
+python hammer.py
+
+
+;;
+
+
+
+12) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pkg install curl
+
+pkg install git
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+cd RED_HAWK
+
+chmod +x rhawk.php
+
+php rhawk.php
+
+
+;;
+
+
+
+13) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install curl
+
+pip2 install -r requiresement.txt
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/Hider5/Malicious
+
+cd Malicious
+
+apt-get install python2
+
+pip2 install -r requirements.txt
+
+python2 malicious.py
+
+
+;;
+
+
+14) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pkg install openssl curl
+
+pkg install git
+
+git clone https://github.com/Ubaii/script-deface-creator
+
+cd script-deface-creator
+
+chmod +x create.py
+
+python2 create.py
+
+
+;;
+
+
+
+15) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install nano
+
+pkg install git
+
+git clone https://github.com/evait-security/weeman.git
+
+cd weeman
+
+python2 weeman.py
+
+
+;;
+
+
+
+16) figlet -f shadow installing...
+
+apt update && apt upgrade
+
+apt install python2
+
+apt install apache2
+
+apt install php
+
+apt install git
+
+git clone https://github.com/Senitopeng/PhisingGame.git
+
+cd PhisingGame
+
+ls
+
+python2 phising.py
+
+
+;;
+
+
+
+17) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pkg install bash -y
+
+pkg install git
+
+git clone https://github.com/BangZatters/BruteDeface-curl
+
+cd BruteDeface-curl
+
+bash Brute
+
+
+;;
+
+
+
+18) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install curl
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/kereh/VulnScaner
+
+cd VulnScaner
+
+chmod +x VulnScaner.py
+
+python2 VulnScaner.py
+
+
+;;
+
+
+
+19) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install curl
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/Dfv47/mbw
+
+cd mbw
+
+sh mbw.sh
+
+
+;;
+
+
+
+20) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install git
+
+git clone https://github.com/AdheBolo/AdminFinder
+
+cd AdminFinder
+
+chmod 777 AdminFinder.py
+
+python2 AdminFinder.py
+
+
+;;
+
+
+21) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install curl
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/derv82/wifite
+
+cd wifite
+
+python2 wifite.py
+
+
+;;
+
+
+
+22) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install curl
+
+pkg install git
+
+git clone https://github.com/Xi4u7/A-Rat.git
+
+cd A-Rat
+
+python2 A-Rat.py
+
+
+;;
+
+
+
+23) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pip2 install mechanize && pip2 install requests
+
+git clone https://github.com/hnov7/mbf
+
+cd mbf
+
+python2 mbf.py
+
+
+;;
+
+
+
+24) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install figlet
+
+pip2 install mechanize && pip2 install requests
+
+pkg install git
+
+git clone
+
+https://github.com/tikuskecil/multi-bruteforce-facebook
+
+cd multi-bruteforce-facebook
+
+python2 MBF.py
+
+
+;;
+
+
+
+25) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pip2 install -r requiresement.txt
+
+pkg install git
+
+git clone https://github.com/AlatToll/cracker.git
+
+cd cracker
+
+pip2 -r requirements.txt
+
+bash cracker.sh
+
+
+;;
+
+
+
+26) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install nano
+
+pkg install git
+
+git clone https://github.com/CiKu370/OSIF.git
+
+cd OSIF
+
+pip2 install -r requirements.txt
+
+python2 osif.py
+
+
+;;
+
+
+
+27) figlet -f shadow installing...
+
+apt update && apt upgrade
+
+apt install git
+
+apt install python2
+
+pip2 install --upgrade pip -y
+
+pkg install php
+
+git clone https://github.com/Ranginang67/DarkFly-Tool
+
+chmod +x install.py
+
+python2 install py
+
+;;
+
+
+28) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/Senitopeng/SpamSms.git
+
+cd SpamSms
+
+python2 mantan.py
+
+
+;;
+
+
+
+29) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pkg install bash
+
+pkg install gnupg
+
+pkg install git
+
+pkg install nano
+
+git clone https://github.com/LionSec/katoolin.git
+
+cd katoolin
+
+nano katoolin.py
+
+
+;;
+
+
+
+30) figlet -f shadow installing...
+
+pkg install python2
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/siputra12/prank
+
+cd prank
+
+php call.php
+
+php wa.php
+
+
+;;
+
+
+
+31) figlet -f shadow installing...
+
+apt update && apt upgrade
+
+pkg install figlet
+
+pkg install lolcat
+
+pkg install ruby
+
+pkg install gem
+
+gem install lolcat
+
+pkg install unzip
+
+pkg install python
+
+pkg install python2
+
+pkg install git
+
+git clone https://github.com/Rusmana-ID/rus
+
+cd rus
+
+unzip bch.zip
+
+cd bch
+
+python2 bch.py
+
+
+
+;;
+
+
+
+*) echo -e $red "Pilih Yang Bener KimaK Jangan Salah ea :v..."
+
+esac
+
